9225134378e16c5eb8c978c2569687c37036106f
Levi_cc <levi.chinecherem@yahoo.com>
2022-06-28 13:20:28 +0100

    initial commit

---
+++ crypto.py
@@ -0,0 +1,22 @@
+# Import libraries
+import json
+import requests
+  
+# Defining Binance API URL
+key = "https://api.binance.com/api/v3/ticker/price?symbol="
+  
+# Making list for multiple crypto's
+currencies = ["BTCUSDT", "DOGEUSDT", "LTCUSDT", "BNBUSDT", 
+              "ETHUSDT",  "ADAUSDT", "LINKUSDT", "VETUSDT", 
+              "XTZUSDT", "BCHUSDT"]
+j = 0
+  
+# running loop to print all crypto prices
+for i in currencies:
+    
+    # completing API for request
+    url = key+currencies[j]  
+    data = requests.get(url)
+    data = data.json()
+    j = j+1
+    print(f"{data['symbol']} price is {data['price']}")
